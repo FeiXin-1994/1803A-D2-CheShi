b9c39e510046481b21e7924535e9d94f8caf57ef
FeiXin-1994 <feixin_19980408@163.com>
2020-11-24 21:42:04 +0800

    张鑫

--- CheShi.Api/Startup.cs
+++ CheShi.Api/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -18,13 +20,38 @@ namespace CheShi.Api
         {
             Configuration = configuration;
         }
+        public void ConfigureContainer(Autofac.ContainerBuilder Cb)
+        {
+            var A = Assembly.GetExecutingAssembly();
+            Cb.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Namespace == "")
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
 
+            Cb.RegisterAssemblyTypes(A)
+                .Where(t => typeof(ControllerBase).IsAssignableFrom(t) && t != typeof(ControllerBase))
+                .PropertiesAutowired();
+        }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddControllers().AddControllersAsServices();
+
+            //允许一个或多个来源可以跨域
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CustomCorsPolicy", policy =>
+                {
+                    // 设定允许跨域的来源，有多个可以用','隔开
+                    policy.WithOrigins("http://localhost:52179")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +61,7 @@ namespace CheShi.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors("CustomCorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
